9038e29e61502e0a4cc76249bb96767feb072a56
cusmoritz <cus.moritz@gmail.com>
2022-11-22 15:13:21 -0700

    drawing snake attempt #2

--- src/Grid.js
+++ src/Grid.js
@@ -8,37 +8,52 @@ const Grid = ({grid, setGrid, color, snake, setSnake, direction, setDirection}) 
 
     let rows = [];
 
-    
-
     const buildBoard = () => {
         for (let rowsInTable = 0; rowsInTable < grid; rowsInTable++){
             // create rows
             let cell = [];
             for (let cellsInRow = 0; cellsInRow < grid; cellsInRow++){
                 //create cells
-                cell.push(<td id={cellsInRow} key={cellsInRow}></td>)
+                cell.push(<td id={[rowsInTable + ", " + cellsInRow]} key={cellsInRow}></td>)
             }
             // add cells to rows
             rows.push(<tr id={rowsInTable} key={rowsInTable}>{cell}</tr>)
         }
-        drawSnake();
     };
 
-    //draw snake function
-    const drawSnake = () => {
+    // mapping over grid to draw snake instead
+
+
+    const Snake = ({snake}) => {
+        console.log(snake)
+        //draw snake function
+        const drawSnake = (snake) => {
         snake.forEach(segment => {
-            console.log('segment here', segment)
-            // console.log(rows[segment[0]]);
-            // console.log(rows[segment[1]]);
-            const row = rows[segment[0]];
-            console.log(`this is row ${segment[0]}`, row);
-            const piece = row.props.children[segment[1]];
-            console.log('this is piece', piece);
-            piece.props.className="snake";
-            // at segment[0] and segment[1]
+            console.log('segment', segment)
+            console.log('this mess', rows)
+            const rowsss = rows[segment[0]];
+            console.log('this mess inside', rowsss)
+            console.log('this mess double nested', rows[segment[0].props])
+
+            return (
+                <>
+                <td className="snake" >{segment}</td>
+                </>
+
+            );
             
-            // console.log('this might be piece', piece)     
+            // console.log('segment here', segment)
+            // // console.log(rows[segment[0]]);
+            // // console.log(rows[segment[1]]);
+            // const row = rows[segment[0]];
+            // console.log(`this is row ${segment[0]}`, row);
+            // const piece = row.props.children[segment[1]];
+            // console.log('this is piece', piece);
+
         });
+    };
+    drawSnake(snake);
+
     };
 
 
@@ -47,13 +62,16 @@ const Grid = ({grid, setGrid, color, snake, setSnake, direction, setDirection}) 
     return (
             <table>
                 <tbody>
+
                 {/* add rows to board */}
                 {rows}
-                
+                <Snake snake={snake} grid={grid}/>
                 </tbody>
             </table>
     )
 
-}
+};
+
+
 
 export default Grid;
